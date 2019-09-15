44bcbe2aea35723c43cbcc6ab9f51d1ff2d9b5d1
Zhao Kang 康昭 <sckangzjobs@gmail.com>
2019-09-15 15:45:04 +0800

    Update run.m

--- run.m
+++ run.m
@@ -1,9 +1,9 @@
-f=load('Reuters.mat');%=load('C:\Users\User\Desktop\research\multiviewlearning\shiguoxin\bbc_seg14of4.mat');
+f=load('reuters_1200.mat');%=load('C:\Users\User\Desktop\research\multiviewlearning\shiguoxin\bbc_seg14of4.mat');
 data=f.data;
 label=f.labels;
 % addpath('C:\Users\User\Desktop\research\kernelclusteringexp')
-para1=[.01 .1 1];
-para2=[100,500,1000,1500,2000];
+para1=[.01  1];
+para2=[100,1000,2000];
 para3=[.01];
 
 for i=1:size(data,1)
